80b1089cc1a5dba2bdecacdbd58ae47bc09d97db
Pauline Pettersson <pauline-pettersson@hotmail.com>
2026-02-17 11:15:00 +0100

    install axios

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 'use strict';
 
-document.querySelector()
+class Pet {
+    constructor(name, animalType) {
+        this.name = name;
+        this.animalType = animalType;
+        this.energy = 50;
+        this.fullness = 50;
+        this.happiness = 50;
+    }
+}
+
+const pet = new Pet('Elton', 'Dog');
+console.log(pet)
