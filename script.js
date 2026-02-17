774fcbafc09cbd1160bb8270f1959bd7b42db3f3
Pauline Pettersson <pauline-pettersson@hotmail.com>
2026-02-17 11:46:39 +0100

    class Pet

--- script.js
+++ script.js
@@ -8,7 +8,42 @@ class Pet {
         this.fullness = 50;
         this.happiness = 50;
     }
+
+    nap() {
+        this.energy += 40;
+        this.fullness -= 10;
+        this.happiness -= 10;
+        // Kontrollerar värdena är mellan 0-100
+        this.progressValues();
+    }
+
+    play() {
+        this.energy -= 10;
+        this.fullness -= 10;
+        this.happiness += 30;
+        this.progressValues();
+    }
+
+    eat() {
+        this.energy -= 15;
+        this.fullness += 30;
+        this.happiness += 5;
+        this.progressValues();
+    }
+
+    // Math.max returnerar det största av talen jag skickar in
+    // Math.min returnerar det minsta
+    // Math.max(0, -10) = 0
+    // Math.min(100, 140) = 100
+    progressValues() {
+        this.energy = Math.min(100, Math.max(0, this.energy));
+        this.fullness = Math.min(100, Math.max(0, this.fullness));
+        this.happiness = Math.min(100, Math.max(0, this.happiness));
+    }
 }
 
 const pet = new Pet('Elton', 'Dog');
 console.log(pet)
+
+
+
